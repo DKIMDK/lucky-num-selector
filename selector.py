1eaf1b77e4e77102d2a68409c5aadfb67e3eb112
DKIMDK <imdeokey@gmail.com>
2023-05-01 14:48:18 +0900

    fix: fixed invalid syntax error

--- selector.py
+++ selector.py
@@ -4,5 +4,5 @@ def get_lucky_nums():
     return random.sample(range(1,45+1), k= 6)
 if __name__ == '__main__':
     times = int(input('Enter num(1-100): '))
-    for in range(times):
+    for _ in range(times):
         print(get_lucky_nums())
